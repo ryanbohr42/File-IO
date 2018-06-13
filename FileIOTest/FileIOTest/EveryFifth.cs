01280ad3c86a1bd5097e583fee03d803d58ef377
Unknown <ryanbohr42@hotmail.com>
2018-06-13 14:41:07 -0500

    Finished Every Fifth Feature

---
+++ FileIOTest/FileIOTest/EveryFifth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileIOTest
+{
+    class EveryFifth
+    {
+        public EveryFifth()
+        {
+        }
+
+        public void displayFifthNumber(string[] nums)
+        {
+            
+            var counter = 4;
+            while (counter < nums.Length)
+            {
+                Console.WriteLine(nums[counter]);
+                counter += 5;
+            }
+
+        }
+    }
+}
--- FileIOTest/FileIOTest/Program.cs
+++ FileIOTest/FileIOTest/Program.cs
@@ -8,11 +8,12 @@ namespace FileIOTest
     {
         static void Main(string[] args)
         {
-            //string[] lines = System.IO.File.ReadAllLines("C:/Users/Ryan/Documents/Ryan/Full Stack Camp/Repos/File IO/FileIOTest/numbers.txt");
+            EveryFifth Ef = new EveryFifth();
+
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             path += "/numbers.txt";
             string[] lines = System.IO.File.ReadAllLines(path);
-            Console.WriteLine(lines[0]);
+            Ef.displayFifthNumber(lines);
             Console.ReadKey();
         }
 
