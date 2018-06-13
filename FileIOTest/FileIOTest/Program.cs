1a6a34a618c1708c6a701849a87d7d062bf650e5
Unknown <ryanbohr42@hotmail.com>
2018-06-13 14:53:37 -0500

    Displayed Whole List

--- FileIOTest/FileIOTest/Program.cs
+++ FileIOTest/FileIOTest/Program.cs
@@ -8,11 +8,10 @@ namespace FileIOTest
     {
         static void Main(string[] args)
         {
-            //string[] lines = System.IO.File.ReadAllLines("C:/Users/Ryan/Documents/Ryan/Full Stack Camp/Repos/File IO/FileIOTest/numbers.txt");
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             path += "/numbers.txt";
             string[] lines = System.IO.File.ReadAllLines(path);
-            Console.WriteLine(lines[0]);
+            displayWholeList.DisplayWholeList(lines);
             Console.ReadKey();
         }
 
---
+++ FileIOTest/FileIOTest/displayWholeList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileIOTest
+{
+    class displayWholeList
+    {
+        public static void DisplayWholeList(string[] numbers)
+        {
+
+
+
+            Console.WriteLine("whole list of input:");
+
+            for (int n = 0; n < numbers.Length; n++)
+            {
+
+                Console.WriteLine(numbers[n]);
+
+            }
+        }
+}
+}
