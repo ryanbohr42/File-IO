025e0f167e1bd6cd9f56b09f8728e304f5b30b41
Unknown <19trant@elmbrookstudents.org>
2018-06-13 14:36:45 -0500

    Display all evens

--- FileIOTest/FileIOTest/Program.cs
+++ FileIOTest/FileIOTest/Program.cs
@@ -9,10 +9,12 @@ namespace FileIOTest
         static void Main(string[] args)
         {
             //string[] lines = System.IO.File.ReadAllLines("C:/Users/Ryan/Documents/Ryan/Full Stack Camp/Repos/File IO/FileIOTest/numbers.txt");
+            Odds o = new Odds();
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             path += "/numbers.txt";
             string[] lines = System.IO.File.ReadAllLines(path);
             Console.WriteLine(lines[0]);
+            o.displayOdds(lines);
             Console.ReadKey();
         }
 
---
+++ FileIOTest/FileIOTest/odds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Odds
+{
+	public Odds()
+	{
+
+	}
+    public void displayOdds(String[] file) 
+    {
+        foreach(String num in file)
+        {
+            if (Int32.Parse(num)% 2 != 0)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
