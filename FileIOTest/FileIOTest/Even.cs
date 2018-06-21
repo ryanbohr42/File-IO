e8244342bf800ea91d4764dfd8daa5fde3b0b358
Ryan Bohr <ryanbohr42@hotmail.com>
2018-06-21 09:33:57 -0500

    fixed output

--- FileIOTest/FileIOTest/Even.cs
+++ FileIOTest/FileIOTest/Even.cs
@@ -14,7 +14,6 @@ namespace FileIOTest
                 if (int.Parse(line)%2 == 0)
                     Console.WriteLine(int.Parse(line));
             }
-            Console.ReadKey();
         }
     }
 }
